927300f3c0935dafc67999fe77a5f80d10740329
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-10-22 10:44:12 +0100

    ephemeral keystore: Add heading

--- crates/tor-keymgr/src/keystore/ephemeral.rs
+++ crates/tor-keymgr/src/keystore/ephemeral.rs
@@ -21,6 +21,8 @@ type KeyIdent = (ArtiPath, KeyType);
 /// are never written to disk, and are stored in-memory as [`SshKeyData`].
 /// Keys saved in this Keystore do not persist between restarts!
 ///
+/// ### Security properties
+///
 /// While Arti never writes the keys for this key store to disk, the operating
 /// system may do so for reasons outside of this library's control. Some
 /// examples are swapping RAM to disk, generating core dumps, invoking
