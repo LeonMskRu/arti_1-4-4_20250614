8e12a47e9a3039979c4ef7002c47089915b394e3
Q Misell <q@magicalcodewit.ch>
2024-11-04 15:19:03 +0100

    document ASN.1 expect safety and OIDs

--- crates/tor-hsservice/src/acme.rs
+++ crates/tor-hsservice/src/acme.rs
@@ -54,6 +54,8 @@ pub(crate) fn onion_csr(
     drop(rng);
 
     // See RFC 2986 for format details
+    // EXPECT SAFETY: ASN.1 serialization shouldn't fail in this function because IO errors can`t
+    // happen when writing to Vec and no DER objects are manually constructed.
 
     // CertificationRequestInfo SEQUENCE
     let mut tbs_csr_contents = Vec::new();
@@ -67,16 +69,13 @@ pub(crate) fn onion_csr(
 
     let mut subject_pk_contents = Vec::new();
     // algorithm AlgorithmIdentifier
-    asn1_rs::Sequence::from_iter_to_der(
-        [
-            // algorithm OBJECT IDENTIFIER - id-Ed25519
-            asn1_rs::oid!(1.3.101 .112),
-        ]
-        .iter(),
-    )
-    .expect("create algorithm AlgorithmIdentifier")
-    .write_der(&mut subject_pk_contents)
-    .expect("serialize algorithm AlgorithmIdentifier");
+    asn1_rs::Sequence::from_iter_to_der([
+        // algorithm OBJECT IDENTIFIER: {iso(1) identified-organization(3) thawte(101) id-Ed25519(112)}
+        asn1_rs::oid!(1.3.101.112),
+    ].iter())
+        .expect("create algorithm AlgorithmIdentifier")
+        .write_der(&mut subject_pk_contents)
+        .expect("serialize algorithm AlgorithmIdentifier");
     // subjectPublicKey BIT STRING
     asn1_rs::BitString::new(0, &hs_key.public().to_bytes())
         .write_der(&mut subject_pk_contents)
@@ -87,8 +86,8 @@ pub(crate) fn onion_csr(
         .expect("serialize subjectPKInfo SubjectPublicKeyInfo");
 
     let mut ca_nonce_contents = Vec::new();
-    // type OBJECT IDENTIFIER - cabf-caSigningNonce
-    asn1_rs::oid!(2.23.140 .41)
+    // type OBJECT IDENTIFIER: {joint-iso-itu-t(2) international-organizations(23) ca-browser-forum(140) cabf-caSigningNonce(41)}
+    asn1_rs::oid!(2.23.140.41)
         .write_der(&mut ca_nonce_contents)
         .expect("serialize type OBJECT IDENTIFIER - cabf-caSigningNonce");
     // values SET
@@ -98,8 +97,8 @@ pub(crate) fn onion_csr(
         .expect("serialize values SET");
 
     let mut applicant_nonce_contents = Vec::new();
-    // type OBJECT IDENTIFIER - cabf-applicantSigningNonce
-    asn1_rs::oid!(2.23.140 .42)
+    // type OBJECT IDENTIFIER: {joint-iso-itu-t(2) international-organizations(23) ca-browser-forum(140) cabf-applicantSigningNonce(42)}
+    asn1_rs::oid!(2.23.140.42)
         .write_der(&mut applicant_nonce_contents)
         .expect("serialize type OBJECT IDENTIFIER - cabf-applicantSigningNonce");
     // values SET
@@ -136,16 +135,13 @@ pub(crate) fn onion_csr(
         .write_der(&mut csr_contents)
         .expect("serialize CertificationRequestInfo SEQUENCE");
     // signatureAlgorithm AlgorithmIdentifier
-    asn1_rs::Sequence::from_iter_to_der(
-        [
-            // algorithm OBJECT IDENTIFIER - id-Ed25519
-            asn1_rs::oid!(1.3.101 .112),
-        ]
-        .iter(),
-    )
-    .expect("create signatureAlgorithm AlgorithmIdentifier")
-    .write_der(&mut csr_contents)
-    .expect("serialize signatureAlgorithm AlgorithmIdentifier");
+    asn1_rs::Sequence::from_iter_to_der([
+        // algorithm OBJECT IDENTIFIER: {iso(1) identified-organization(3) thawte(101) id-Ed25519(112)}
+        asn1_rs::oid!(1.3.101.112),
+    ].iter())
+        .expect("create signatureAlgorithm AlgorithmIdentifier")
+        .write_der(&mut csr_contents)
+        .expect("serialize signatureAlgorithm AlgorithmIdentifier");
     // signature BIT STRING
     asn1_rs::BitString::new(0, signature.to_bytes().as_slice())
         .write_der(&mut csr_contents)
