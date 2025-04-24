084bdf01a8a3561af46a94705080354ac30f8f2a
hashcatHitman <3924-hashcatHitman@gitlab.torproject.org>
2025-04-24 04:24:15 -0400

    tor-circmgr: Added MSRV 1.80 TODO for `once_cell` removal
    - Added a TODO for MSRV >= 1.80 reminding us to try to replace the usage of the
    `once_cell` crate with the standard library implementation.

--- crates/tor-circmgr/src/mgr.rs
+++ crates/tor-circmgr/src/mgr.rs
@@ -1546,6 +1546,11 @@ mod test {
     #[allow(deprecated)] // TODO #1885
     use tor_rtmock::MockSleepRuntime;
 
+    // TODO MSRV 1.80: See about replacing this usage of
+    // [`once_cell::sync::Lazy`] with [`std::sync::LazyLock`]. See [1] for more
+    // information.
+    //
+    // [1]: https://doc.rust-lang.org/std/sync/struct.LazyLock.html
     static FALLBACKS_EMPTY: Lazy<FallbackList> = Lazy::new(|| [].into());
 
     fn di() -> DirInfo<'static> {
