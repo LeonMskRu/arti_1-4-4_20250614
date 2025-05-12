da119a494b7d1246f291de2f5191e8aa9fad12a3
hashcatHitman <3924-hashcatHitman@gitlab.torproject.org>
2025-05-12 15:37:38 -0400

    tor-log-ratelim: Modified MSRV 1.80 TODO for `once_cell` removal
    - Shortened the TODO added in 85f507dc330ac9d3c3163d6225458d7f438ede8e as per
    [maintainer request].
    
    [maintainer request]: https://gitlab.torproject.org/tpo/core/arti/-/merge_requests/2953#note_3197719

--- crates/tor-log-ratelim/src/lib.rs
+++ crates/tor-log-ratelim/src/lib.rs
@@ -86,11 +86,7 @@ pub mod macro_prelude {
         ratelim::{rt::rt_support, RateLim},
         Activity, Loggable,
     };
-    // TODO MSRV 1.80: See about replacing this usage of
-    // [`once_cell::sync::Lazy`] with [`std::sync::LazyLock`]. See [1] for more
-    // information.
-    //
-    // [1]: https://doc.rust-lang.org/std/sync/struct.LazyLock.html
+    // TODO MSRV 1.80: Replace with LazyLock (#1996)
     pub use once_cell::sync::Lazy;
     pub use std::sync::{Arc, Mutex, Weak};
     pub use tor_error::ErrorReport;
